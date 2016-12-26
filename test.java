56d29d0acb9d72f0d1e6b191303e706400c75cb8
SiddharthDixit23 <siddharth.23.dixit@gmail.com>
2016-12-27 01:47:37 +0530

    error

---
+++ test.java
@@ -0,0 +1,11 @@
+//If we try to run this code then the compiler will print an error as main method is missing in the code
+
+public class test{
+static{
+print(10);
+}
+static void print (int x) {
+System.out.println (x);
+System.exit(0);
+}
+}
